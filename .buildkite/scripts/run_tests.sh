28f8b3934540e4a02173f0ac999b1e3af59391a4
CerealBoy <allan.shone@gmail.com>
2025-03-25 21:35:30 +1100

    Debug the test run

--- .buildkite/scripts/run_tests.sh
+++ .buildkite/scripts/run_tests.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
+# This should be "/app"
+pwd
+ls -la ./test/
+
+# Remove the warning, it's just noise
+touch .env
+
 if [[ "${MIX_TEST_PARTITION:-0}" = "0" ]]; then
     mix test
 else
