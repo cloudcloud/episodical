3115963f034f5b6bcebf94a69a25c90d5dcf5bcd
CerealBoy <allan@buildkite.com>
2025-03-28 10:39:51 +1100

    I think this is the wrong direction

--- .buildkite/scripts/run_tests.sh
+++ .buildkite/scripts/run_tests.sh
@@ -2,20 +2,22 @@
 
 touch .env
 
-base="$(mix phx.gen.secret 64)"
-enc="$(elixir --eval "IO.puts :crypto.strong_rand_bytes(32) |> :base64.encode")"
-
 if [[ "${TEST_ENGINE_TOKEN:-}" == "" ]]; then
     echo "The 'TEST_ENGINE_TOKEN' environment variable is required."
     exit 1
 fi
 
+enc="$(elixir --eval "IO.puts :crypto.strong_rand_bytes(32) |> :base64.encode")"
 cat << VARS > .env
 export ENCRYPTION_KEYS=${enc}
-export SECRET_KEY_BASE=${base}
 export TEST_ENGINE_TOKEN=${TEST_ENGINE_TOKEN}
 export BUILDKITE_ANALYTICS_TOKEN=${TEST_ENGINE_TOKEN}
 VARS
+
+mix deps.get
+mix assets.setup
+mix assets.build
+mix compile
 
 echo "Currently in '$(pwd)' for running tests..."
 
