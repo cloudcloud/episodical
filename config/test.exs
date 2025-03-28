b44eb4dd48b6492d0f5ccb9c32ee734216ed9a26
CerealBoy <allan.shone@gmail.com>
2025-03-29 09:28:32 +1100

    Try removing the debug again

--- config/test.exs
+++ config/test.exs
@@ -39,7 +39,7 @@ config :phoenix_live_view,
 # Using the test collector for great power
 config :buildkite_test_collector,
   api_key: System.get_env("BUILDKITE_ANALYTICS_TOKEN"),
-  http_debug: true
+  http_debug: false
 
 config :episodical, Episodical.Encryption,
     keys:
