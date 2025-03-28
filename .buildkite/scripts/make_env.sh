c5bf21da456aaf2c220065a71b1b1b57a0cee859
CerealBoy <allan.shone@gmail.com>
2025-03-28 21:26:01 +1100

    Update the build component

--- .buildkite/scripts/make_env.sh
+++ .buildkite/scripts/make_env.sh
@@ -6,6 +6,8 @@ touch .env
 base="iYGKSNr0jT7Z7E6z0PpzWbYiepFEPcw+wVZ+5YkpRcwIRG5lyhsOQe+K01h922Wy"
 enc="$(elixir --eval "IO.inspect :crypto.strong_rand_bytes(32) |> :base64.encode")"
 
+echo "$(pwd) is where this is running from"
+
 cat << VARS > .env
 export ENCRYPTION_KEYS=${enc}
 export SECRET_KEY_BASE=${base}
