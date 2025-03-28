3166dfbdd454e92abe27920ec6c00bb41b7a2166
CerealBoy <allan.shone@gmail.com>
2025-03-28 21:28:52 +1100

    Forgot the key

--- .buildkite/scripts/run_tests.sh
+++ .buildkite/scripts/run_tests.sh
@@ -8,15 +8,11 @@ if [[ "${TEST_ENGINE_TOKEN:-}" == "" ]]; then
 fi
 
 enc="$(elixir --eval "IO.puts :crypto.strong_rand_bytes(32) |> :base64.encode")"
-cat << VARS > .env
+cat << VARS >> .env
 export ENCRYPTION_KEYS=${enc}
+export SECRET_KEY_BASE=iYGKSNr0jT7Z7E6z0PpzWbYiepFEPcw+wVZ+5YkpRcwIRG5lyhsOQe+K01h922Wy
 export BUILDKITE_ANALYTICS_TOKEN=${TEST_ENGINE_TOKEN}
 VARS
-
-mix deps.get
-mix assets.setup
-mix assets.build
-mix compile
 
 echo "Currently in '$(pwd)' for running tests..."
 
