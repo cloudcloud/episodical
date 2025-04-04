f264ff48cd82ba375fa05cec941c3b4eb948f2ea
CerealBoy <allan.shone@gmail.com>
2025-04-04 19:35:06 +1100

    That was the wrong thing I remembered, so remove the env that's missed

--- .buildkite/scripts/build.sh
+++ .buildkite/scripts/build.sh
@@ -3,10 +3,9 @@
 touch .env
 
 base="iYGKSNr0jT7Z7E6z0PpzWbYiepFEPcw+wVZ+5YkpRcwIRG5lyhsOQe+K01h922Wy"
-enc="$(elixir --eval "IO.inspect :crypto.strong_rand_bytes(64) |> :base64.encode")"
+enc="$(elixir --eval "IO.inspect :crypto.strong_rand_bytes(32) |> :base64.encode")"
 
 cat <<CONFIG >.env
-export BUILDKITE_ANALYTICS_TOKEN=${TEST_ENGINE_TOKEN}
 export ENCRYPTION_KEYS=${enc}
 export SECRET_KEY_BASE=${base}
 CONFIG
