bac2be4884a5fb043371326950a6845ba2ffdf7c
CerealBoy <allan.shone@gmail.com>
2025-04-05 23:45:49 +1100

    Prepping, switching, cleaning, fingers crossed

--- .buildkite/scripts/run_tests.sh
+++ .buildkite/scripts/run_tests.sh
@@ -2,25 +2,13 @@
 
 touch .env
 
-base="iYGKSNr0jT7Z7E6z0PpzWbYiepFEPcw+wVZ+5YkpRcwIRG5lyhsOQe+K01h922Wy"
-enc="di39k8ByviRM4HHybXbBDZzhUx/RQfzLbJhb5rdQZ2U="
-
 if [[ "${TEST_ENGINE_TOKEN:-}" == "" ]]; then
   echo "The 'TEST_ENGINE_TOKEN' environment variable is required."
   exit 1
 fi
 
-mix deps.compile
-mix compile
-
-# Provision the database, might be the problem
-mix ecto.create
-echo "Database should now be provisioned."
-
-cat <<CONFIG >.env
+cat <<CONFIG >>.env
 export BUILDKITE_ANALYTICS_TOKEN=${TEST_ENGINE_TOKEN}
-export ENCRYPTION_KEYS=${enc}
-export SECRET_KEY_BASE=${base}
 export TEST_ENGINE_TOKEN=${TEST_ENGINE_TOKEN}
 CONFIG
 
