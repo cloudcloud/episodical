df3b4cf49ae54982457f77f81e91fd6c525254d0
CerealBoy <allan.shone@gmail.com>
2025-04-05 13:19:32 +1100

    Consolidate script

--- .buildkite/scripts/install.sh
+++ .buildkite/scripts/install.sh
@@ -1,10 +1,18 @@
 #!/usr/bin/env bash
 
+# Prep the .env first
+touch .env
+base="iYGKSNr0jT7Z7E6z0PpzWbYiepFEPcw+wVZ+5YkpRcwIRG5lyhsOQe+K01h922Wy"
+enc="RPjCgzeIfm8L2l70HIvcj5kPBhVcCN2SRWSPYR/h7oE="
+
+cat <<CONFIG >.env
+export ENCRYPTION_KEYS=${enc}
+export SECRET_KEY_BASE=${base}
+CONFIG
+
 # Get the deps
 mix deps.get
 
-# Prep the build
-./.buildkite/scripts/build.sh
 # Compile the deps
 mix deps.compile
 
