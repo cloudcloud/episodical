318da672668e41b55040d2526d4d413f2269ae84
CerealBoy <allan.shone@gmail.com>
2025-04-05 13:16:31 +1100

    Clean-up the install script

--- .buildkite/scripts/install.sh
+++ .buildkite/scripts/install.sh
@@ -1,25 +1,16 @@
 #!/usr/bin/env bash
 
-export MIX_ENV="test"
-export LANG="en_US.UTF-8"
-export LANGUAGE="en_US:en"
-export LC_ALL="en_US.UTF-8"
-
-apt-get update -y
-apt-get install -y build-essential git libstdc++6 openssl libncurses5 locales ca-certificates
-
-sed -i '/en_US.UTF-8/s/^# //g' /etc/locale.gen
-locale-gen
-
-mix local.hex --force
-mix local.rebar --force
-
+# Get the deps
 mix deps.get
-mkdir config
 
+# Prep the build
 ./.buildkite/scripts/build.sh
+# Compile the deps
 mix deps.compile
 
+# Prep the assets
 mix assets.setup
+# Build the assets
 mix assets.build
+# Compile episodical
 mix compile
