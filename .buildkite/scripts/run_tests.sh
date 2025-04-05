bb96fc457f4fe7191615a5b95b90b08afae5a53e
CerealBoy <allan.shone@gmail.com>
2025-04-05 17:45:18 +1100

    Perhaps it's the create that's missing

--- .buildkite/scripts/run_tests.sh
+++ .buildkite/scripts/run_tests.sh
@@ -17,14 +17,21 @@ docker run -d \
   postgres:17.4-bookworm
 
 sleep 2
+echo "Checking postgres..."
 if [[ $(pg_isready -h=localhost -p=5432 -U=postgres 2>&1 >/dev/null && echo $?) -ne 0 ]]; then
   echo "Not ready yet, sleep a little longer..."
   sleep 2
+else
+  echo "We can connect to postgres now!"
 fi
+
+# Provision the database, might be the problem
+mix ecto.create
 
 cat <<CONFIG >.env
 export BUILDKITE_ANALYTICS_TOKEN=${TEST_ENGINE_TOKEN}
 export ENCRYPTION_KEYS=${enc}
+export PGHOST=localhost
 export SECRET_KEY_BASE=${base}
 export TEST_ENGINE_TOKEN=${TEST_ENGINE_TOKEN}
 CONFIG
